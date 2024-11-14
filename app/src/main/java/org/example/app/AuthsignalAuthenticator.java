9e6ad85ea968e89971065bcfa651bb9259019bce
Steven Clouston <stevenclouston@gmail.com>
2024-11-14 17:41:58 +1300

    Attempt to use authsignal client

--- app/src/main/java/org/example/app/AuthsignalAuthenticator.java
+++ app/src/main/java/org/example/app/AuthsignalAuthenticator.java
@@ -3,13 +3,37 @@ package com.keycloak.providers;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
+
 import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.Authenticator;
 
+import com.authsignal.model.TrackRequest;
+import com.authsignal.model.TrackResponse;
+import com.authsignal.AuthsignalClient;
+
+import java.util.concurrent.CompletableFuture;
+
 public class AuthsignalAuthenticator implements Authenticator {
+    private String baseUrl = "https:// dev-signal.authsignal.com/v1";
+    private String secret = "nn4OBTWLrdXpc3102b2Ntq+6xEytGsTBjakBqiErRrFJnj2GkPUQsQ==";
+
+    AuthsignalClient authsignalClient = new AuthsignalClient(
+            "nn4OBTWLrdXpc3102b2Ntq+6xEytGsTBjakBqiErRrFJnj2GkPUQsQ==", "https://dev-signal.authsignal.com/v1");
+
     @Override
     public void authenticate(AuthenticationFlowContext context) {
         System.out.println("Authenticating with Authsignal");
+        TrackRequest request = new TrackRequest();
+        request.userId = "userId";
+        request.action = "action";
+
+        try {
+            CompletableFuture<TrackResponse> response = authsignalClient.track(request);
+            context.success();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         context.success();
     }
 
