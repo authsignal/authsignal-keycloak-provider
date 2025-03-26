e505a8b12cf755fdf5d85094ced94568e61c667a
Steven Clouston <stevenclouston@gmail.com>
2025-03-27 12:37:59 +1300

    Handle email sign-in (#16)

--- app/src/main/java/com/authsignal/keycloak/AuthsignalAuthenticator.java
+++ app/src/main/java/com/authsignal/keycloak/AuthsignalAuthenticator.java
@@ -82,6 +82,10 @@ public class AuthsignalAuthenticator implements Authenticator {
     UserModel user = context.getSession().users().getUserByUsername(context.getRealm(), username);
 
     if (user == null) {
+        user = context.getSession().users().getUserByEmail(context.getRealm(), username);
+    }
+
+    if (user == null) {
         logger.warning("User not found");
         context.failureChallenge(AuthenticationFlowError.INVALID_USER, context.form()
             .setError("Invalid username or password")
